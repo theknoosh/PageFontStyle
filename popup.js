e7a286d0c0b41874c4bd7e72695bcfe768e270a4
Darrell Payne <d.anthony.payne@gmail.com>
2017-06-15 20:13:12 -0700

    Color issue fixed

--- popup.js
+++ popup.js
@@ -4,7 +4,8 @@ $(function(){
         color = $(this).val();
     });
     $('#btnChange').click(function(){
-        chrome.tabs.query({active: true, currentWindow: true}, function(){
+        // alert("test");
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
             chrome.tabs.sendMessage(tabs[0].id, {todo: "changeColor", clickedColor:color})
         });
     });
